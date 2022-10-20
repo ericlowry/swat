76a64fea68b35b33690ef69eaaff40e605b8652a
Eric Lowry <eric@myrealms.org>
2022-10-20 10:54:03 -0600

    navigate authenticate users to /home

--- client/src/components/Landing/Landing.js
+++ client/src/components/Landing/Landing.js
@@ -1,3 +1,14 @@
+import useAuth from '../../state/useAuth';
+
+import { Link, Navigate } from 'react-router-dom';
+
 export default function Landing() {
-  return <h1>Landing</h1>;
+  const { profile } = useAuth();
+  if (profile) return <Navigate to="/home" />;
+  return (
+    <>
+      <h1>Welcome To {process.env.REACT_APP_CLIENT_BRAND}</h1>
+      <p>Perhaps you want to <Link to="/login">login</Link>?</p>
+    </>
+  );
 }
